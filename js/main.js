e03dbf9f904cca720337134c333e588f9e088ef3
dimakovalev25 <d.nozdryn@gmail.com>
2022-07-25 23:02:13 +0300

    + carousel

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-var button = document.querySelector("button");
-/*console.log(document);   выводить в консоль */
-button.addEventListener('click', function (event) {
-  console.log("клик по кнопке!");  /*  отслеживаем клики по кнопке */
-  event.target.classList.add("active"); /* вешаем кнопке класс active */
-  event.target.classList.remove("active");  /* удаляем класс */
-  event.target.classList.toggle("active");  /*  проверка,если класса нет-добавляеться, если класс
-   есть -то удаляеться! */
-});  
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect: 'coverflow',
+});
 
